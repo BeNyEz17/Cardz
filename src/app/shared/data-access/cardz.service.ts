0afcb8169be653e3df18ef92773c1e468798d6d7
BeNyEz17 <benjaminpluchart10@gmail.com>
2023-01-04 11:59:27 +0100

    MSJ

--- src/app/shared/data-access/cardz.service.ts
+++ src/app/shared/data-access/cardz.service.ts
@@ -1,8 +1,16 @@
-import {Injectable} from "@angular/core";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {HttpClient} from "@angular/common/http";
+import {Cardz} from "../../shared/types/cardz";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CardzService {
 
+  constructor(private http: HttpClient) {}
+
+  getCardzById(id: number): Observable<Cardz> {
+    return this.http.get<Cardz>('http://localhost:3000/cardz/1');
+  }
 }
